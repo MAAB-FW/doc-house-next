3c5e3bb60fe59c845e77a1d17c8ce499f934539f
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-08-27 21:25:26 +0600

    add: sign in page added with view height issue,(previous commit was for sign up page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,7 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-    return <div>SignIn Page</div>;
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center md:flex-row">
+            {/* image side */}
+            <div className="relative flex h-[541px] w-full items-end justify-center bg-[#07332f] md:h-[170vh] md:w-1/2 md:items-center">
+                {/* plus icon */}
+                <Image
+                    src="/login/Frame-1.png"
+                    alt=""
+                    width={500}
+                    height={500}
+                    className="absolute right-0 top-0 h-[186px] w-[268px] md:h-[322px] md:w-[464px]"
+                />
+                {/* doctor image */}
+                <Image
+                    src="/login/Frame.png"
+                    alt=""
+                    width={571}
+                    height={572}
+                    className="mb-[71px] h-[272px] w-[271px] md:h-[572px] md:w-[571px]"
+                />
+            </div>
+            {/* form side */}
+            <div className="flex h-full w-full items-center px-5 py-8 md:w-1/2 lg:pl-[121px]">
+                <div className="w-full rounded-[10px] border px-5 py-[30px] text-[#0A0808] md:w-[461px] md:p-[50px]">
+                    <p className="mb-6 text-center text-3xl font-bold md:mb-[50px]">Sign In to Doc House</p>
+                    <form className="flex flex-col gap-4 text-xl font-semibold md:gap-6">
+                        <label htmlFor="username">
+                            Username or Email Address
+                            <input
+                                type="text"
+                                id="username"
+                                className="mt-[10px] block w-full rounded-md border bg-[#f3f3f3] p-5 text-base font-normal focus:border-gray-500 focus:outline-none"
+                                placeholder="Enter your username"
+                            />
+                        </label>
+                        <label htmlFor="password">
+                            Password
+                            <input
+                                type="password"
+                                id="password"
+                                className="mt-[10px] block w-full rounded-md border bg-[#f3f3f3] p-5 text-base font-normal focus:border-gray-500 focus:outline-none"
+                                placeholder="Enter your password"
+                            />
+                        </label>
+                        <button type="submit" className="mt-5 w-full rounded-md bg-[#f7a582] p-5 font-bold text-white">
+                            Sign In
+                        </button>
+                        <p className="text-center text-lg font-normal text-[#6C6B6B]">
+                            Please register at first. Go to{" "}
+                            <Link href="/signup" className="uppercase text-[#f7a582]">
+                                SIGN UP
+                            </Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Page;
