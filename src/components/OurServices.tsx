b8cd882433d413f518164ab609f6ede3ba88e449
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-09-08 20:48:58 +0600

    update: OurServices section are responsive

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -3,23 +3,35 @@ import React from "react";
 
 const OurServices = () => {
     return (
-        <div className="item-center mx-auto my-[130px] flex max-w-[1140px] flex-col justify-center gap-6 md:flex-row">
-            <Image src={"/ourServices/Rectangle 20078.png"} alt="services" width={558} height={1053} className="w-1/2" />
-            <div className="w-1/2">
-                <h2 className="mb-5 text-[40px] font-bold text-[#0A0808]">Our Services</h2>
-                <p className="mb-[30px] w-[438px] text-[#3B3A3A]">
+        <div className="item-center mx-auto my-8 flex flex-col justify-center gap-6 lg:my-[130px] lg:max-w-[1140px] lg:flex-row">
+            <Image
+                src={"/ourServices/Rectangle 20078.png"}
+                alt="services"
+                width={558}
+                height={1053}
+                className="mx-auto max-h-[633px] max-w-[335px] md:max-h-[1053px] md:w-1/2 md:max-w-[558px]"
+            />
+            <div className="mx-auto w-[335px] text-center md:w-1/2 lg:text-left">
+                <h2 className="mb-4 text-[30px] font-bold text-[#0A0808] md:mb-5 md:text-[40px]">Our Services</h2>
+                <p className="mb-4 text-sm text-[#3B3A3A] md:mb-[30px] md:text-base lg:w-[438px]">
                     Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
                     aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                 </p>
-                <div className="text-[#3B3A3A ] mb-[50px] flex items-center justify-between rounded-[10px] border border-[#E6E6E6] text-lg font-bold text-[#3B3A3A] *:rounded-[10px] *:px-4 *:py-[27px]">
+                <div className="mb-4 flex items-center justify-between rounded-[10px] border border-[#E6E6E6] text-xl font-bold text-[#3B3A3A] *:rounded-[10px] *:px-4 *:py-[27px] md:mb-[50px] md:text-lg">
                     <button className="bg-[#F7A582]">Cavity Protection</button>
                     <button>Cosmetic Dentisty</button>
                     <button>Oral Surgery</button>
                 </div>
-                <Image src={"/ourServices/Rectangle 10.png"} alt="services" width={558} height={350} className="mb-[30px]" />
-                <h2 className="mb-5 text-3xl font-bold text-[#0A0808]">Electro Gastrology Therapy</h2>
-                <div className="mb-5 space-y-[10px] text-[#3B3A3A]">
-                    <p className="mb-[10px]">
+                <Image
+                    src={"/ourServices/Rectangle 10.png"}
+                    alt="services"
+                    width={558}
+                    height={350}
+                    className="mb-4 md:mb-[30px]"
+                />
+                <h2 className="mb-4 text-3xl font-bold text-[#0A0808] md:mb-5">Electro Gastrology Therapy</h2>
+                <div className="mb-4 space-y-[10px] text-[#3B3A3A] md:mb-5">
+                    <p className="mb-4 md:mb-[10px]">
                         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
                         rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt
                         explicabo. Sed ut perspiciatis unde omnis iste natus error
