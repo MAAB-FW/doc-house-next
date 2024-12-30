769b95135e717dee11098a2f1bbe04b225b1edb0
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-12-30 21:13:13 +0600

    update: fixed the responsiveness for mobile device

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from "react";
 
 const OurServices = () => {
     return (
@@ -9,15 +8,15 @@ const OurServices = () => {
                 alt="services"
                 width={558}
                 height={1053}
-                className="mx-auto max-h-[633px] max-w-[335px] md:max-h-[1053px] md:w-1/2 md:max-w-[558px]"
+                className="mx-auto max-h-[633px] max-w-[300px] md:max-h-[1053px] md:w-1/2 md:max-w-[558px]"
             />
-            <div className="mx-auto w-[335px] text-center md:w-1/2 lg:text-left">
+            <div className="mx-auto w-[300px] text-center md:w-1/2 lg:text-left">
                 <h2 className="mb-4 text-[30px] font-bold text-[#0A0808] md:mb-5 md:text-[40px]">Our Services</h2>
                 <p className="mb-4 text-sm text-[#3B3A3A] md:mb-[30px] md:text-base lg:w-[438px]">
                     Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
                     aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                 </p>
-                <div className="mb-4 flex items-center justify-between rounded-[10px] border border-[#E6E6E6] text-xl font-bold text-[#3B3A3A] *:rounded-[10px] *:px-4 *:py-[27px] md:mb-[50px] md:text-lg">
+                <div className="*:p x-4 mb-4 flex items-center justify-between rounded-[10px] border border-[#E6E6E6] text-xl font-bold text-[#3B3A3A] *:rounded-[10px] *:py-[27px] md:mb-[50px] md:text-lg">
                     <button className="bg-[#F7A582]">Cavity Protection</button>
                     <button>Cosmetic Dentisty</button>
                     <button>Oral Surgery</button>
