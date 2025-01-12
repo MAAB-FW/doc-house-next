575689e89afc19dd45ba85b8ec4d5394747cf2ff
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-01-12 19:10:59 +0600

    update: Service card section added

---
+++ src/components/ServiceCard.tsx
@@ -0,0 +1,12 @@
+import { TServiceCard } from "@/types/types";
+
+export default function ServiceCard({ title, color, icon }: TServiceCard) {
+    return (
+        <div className="mx-auto flex w-[366px] items-center gap-[1.5625rem] rounded-[0.625rem] p-[1.5625rem] shadow-md">
+            <div style={{ backgroundColor: color, padding: "1.5625rem" }} className="rounded-[0.625rem]">
+                {icon}
+            </div>
+            <p className="text-[1.5625rem] font-bold text-[#3B3A3A]">{title}</p>
+        </div>
+    );
+}
