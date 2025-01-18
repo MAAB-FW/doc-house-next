46fd5e18ad67902674e633a328c4950e023f9ee2
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-01-18 19:20:56 +0600

    fixed: rating star color showing fixed

--- src/components/DoctorCard.tsx
+++ src/components/DoctorCard.tsx
@@ -1,7 +1,6 @@
 import type { TDoctorCard } from "@/types/types";
 import Image from "next/image";
 
-
 export default function DoctorCard({ name, profession, ratings, location, availability, price, doctorImage }: TDoctorCard) {
     return (
         <div className="space-y-5">
@@ -26,13 +25,13 @@ export default function DoctorCard({ name, profession, ratings, location, availa
                             height="24"
                             viewBox="0 0 24 24"
                             fill="none"
-                            className={`h-5 w-5 ${index < ratings ? "text-yellow-500" : "text-gray-300"}`}
+                            className={"h-5 w-5"}
                         >
                             <path
                                 fill-rule="evenodd"
                                 clip-rule="evenodd"
                                 d="M10.788 3.21009C11.236 2.13309 12.764 2.13309 13.212 3.21009L15.294 8.21709L20.698 8.65009C21.862 8.74309 22.334 10.1951 21.447 10.9551L17.33 14.4821L18.587 19.7551C18.858 20.8911 17.623 21.7881 16.627 21.1801L12 18.3541L7.373 21.1801C6.377 21.7881 5.142 20.8901 5.413 19.7551L6.67 14.4821L2.553 10.9551C1.666 10.1951 2.138 8.74309 3.302 8.65009L8.706 8.21709L10.788 3.21109V3.21009Z"
-                                fill="#F2871D"
+                                fill={`${index < ratings ? "#F2871D" : "#CECECE"}`}
                             />
                         </svg>
                     ))}
