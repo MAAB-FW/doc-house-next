62e322ceed85bcce34189038cacbd87e9f90aab7
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-03-19 19:16:04 +0600

    update: table responsive ratio adjust

--- src/app/(dashboard)/dashboard/MyAppointment/page.tsx
+++ src/app/(dashboard)/dashboard/MyAppointment/page.tsx
@@ -6,9 +6,9 @@ export default function MyAppointment() {
                 <p className="rounded-lg border border-black p-3">MAY 10, 2022</p>
             </div>
             <div className="mt-5 w-full overflow-x-auto rounded-xl">
-                <table className="w-full overflow-hidden rounded-xl bg-[#FFFFFF] text-left">
+                <table className="w-full overflow-hidden rounded-xl bg-[#FFFFFF]">
                     <thead>
-                        <tr className="bg-[#E6E6E6] text-sm uppercase *:min-w-8 *:px-6 *:py-3">
+                        <tr className="bg-[#E6E6E6] text-left text-sm uppercase *:min-w-8 *:px-4 *:py-2 *:lg:py-3">
                             <th></th>
                             <th>Name</th>
                             <th>Service</th>
@@ -17,8 +17,8 @@ export default function MyAppointment() {
                     </thead>
                     <tbody>
                         {Array.from(Array(5).keys()).map((_, idx) => (
-                            <tr key={idx} className="text-nowrap *:px-6 *:py-4">
-                                <td className="">{idx + 1}</td>
+                            <tr key={idx} className="text-nowrap text-left text-xs *:px-4 *:py-4 lg:text-sm">
+                                <td className="text-center">{idx + 1}</td>
                                 <td>John Doe</td>
                                 <td>Teeth Orthodontics</td>
                                 <td>08.30 AM - 09.00 AM</td>
