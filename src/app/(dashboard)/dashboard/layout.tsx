a5284382cb292048c5055ab202536dedd593039b
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2025-03-24 22:59:03 +0600

    update: responsive issue, viewport adjust

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -5,13 +5,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="bg-[#f1f5f9]">
             <DashNav />
-            <div className="flex flex-col lg:flex-row">
+            <div className="flex h-screen flex-col lg:h-full lg:flex-row">
                 <div className="hidden min-h-[calc(100vh-90px)] w-[26%] max-w-[303px] bg-white lg:inline">
                     <ul className="ml-9 mt-5">
                         <li>My Appointment</li>
                     </ul>
                 </div>
-                <div className="mx-14 my-6 min-h-[calc(100vh-138px)] flex-1">{children}</div>
+                <div className="mx-1 my-6 min-h-[calc(100vh-138px)] flex-1 sm:mx-14">{children}</div>
             </div>
         </div>
     );
